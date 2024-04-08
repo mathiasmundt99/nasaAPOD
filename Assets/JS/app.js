8e4dc09b2c54fae2941a5846b3ff7f1c7eb2e5b7
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-04-08 20:05:47 +0200

    getAPOD created

---
+++ Assets/JS/app.js
@@ -0,0 +1,25 @@
+const apiKey = 'aKrnFGwbPncYK65yfb3C7bz8nRuedH4d7p3OhC9x';
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+const articleEl = document.querySelector('.apodContainer');
+
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => getAPOD(data))
+  .catch(err => console.log('Error', err));
+
+function getAPOD(apod){
+    console.log('apod', apod)
+    const newArticle = document.createElement('article');
+    const newH2 = document.createElement('h2')
+    const newImg = document.createElement('img');
+    const newDescription = document.createElement('p')
+
+    newH2.textContent = apod.title
+    newDescription.textContent = apod.explanation
+    newImg.src = apod.hdurl;
+    newArticle.append(newH2, newImg, newH2, newDescription);
+    console.log(newArticle)
+
+    articleEl.append(newArticle);
+};
